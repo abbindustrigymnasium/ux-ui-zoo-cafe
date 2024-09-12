65ded70809f2237cbe8b44422115dfb0f0bba022
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-09-12 11:12:34 +0200

    Started working on card element for order menu

---
+++ src/pages/orderMenuPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+ import theImage from "assets/cool.jpg"
+</script>
+
+
+
+<template>
+    <q-card-selection class=" shadow-md p-4 flex rounded-lg w-56 h-64 bg-white">
+        <div class=" h-[calc(100%-4rem)] w-full">
+            <img :src="theImage" alt="image of product" class="h-full w-full object-fill rounded">
+        </div>
+
+       <div class="flex w-full">
+            <div>
+                <div class=" " >Mat</div>
+                <div>price kr</div>
+            </div>
+            <button class=" bg-black text-white px-4 rounded ml-auto" >ADD TO CART</button>
+       </div>
+    </q-card-selection>
+</template>
