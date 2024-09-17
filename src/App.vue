b0976d41ab58f8a0d4608d563bb943ecf395bdff
Adrian Damianovici <adrian.damianovici@hitachigymnasiet.se>
2024-09-17 11:12:43 +0200

    Started styling Text for order card

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  .bebas-neue-regular {
+    font-family: "Bebas Neue", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+  
+  
+
+  body{
+    background-color: #FEF8E5;
+  }
 </style>
 <template>
   <router-view />
@@ -11,4 +23,7 @@
 defineOptions({
   name: 'App'
 });
+
+
+
 </script>
--- src/pages/orderMenuPage.vue
+++ src/pages/orderMenuPage.vue
@@ -5,17 +5,17 @@
 
 
 <template>
-    <q-card-selection class=" shadow-md p-4 flex rounded-lg w-56 h-64 bg-white">
+    <q-card-selection class=" shadow-lg p-4 flex rounded-lg w-56 h-64 bg-white">
         <div class=" h-[calc(100%-4rem)] w-full">
             <img :src="theImage" alt="image of product" class="h-full w-full object-fill rounded">
         </div>
 
        <div class="flex w-full">
             <div>
-                <div class=" " >Mat</div>
+                <div class="bebas-neue-regular text-4xl" >Mat</div>
                 <div>price kr</div>
             </div>
-            <button class=" bg-black text-white px-4 rounded ml-auto" >ADD TO CART</button>
+            <button class=" bg-black text-white px-4 rounded ml-auto" >ADD TO CART</button> <!--TODO: Fix Heiht of button-->
        </div>
     </q-card-selection>
 </template>
